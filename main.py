ba3020ab9e7d242ed6d9593e917caffd3cf59237
ziyit926 <168748295+ziyit926@users.noreply.github.com>
2025-05-17 17:45:54 +0100

    added import, created training and testing dataset to main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+# Define batch size and number of epochs
+batch_size = 4
+num_epochs = 100
+
+# Ensure the training will only happen in CPU.
+device = torch.device('cpu')
+
+# Use transforms.compose method to reformat images for modeling
+all_transforms = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                     ])
+# Create Training dataset
+train_dataset = torchvision.datasets.CIFAR10(root = './data',
+                                             train = True,
+                                             transform = all_transforms,
+                                             download = True)
+
+# Create Testing dataset
+test_dataset = torchvision.datasets.CIFAR10(root = './data',
+                                            train = False,
+                                            transform = all_transforms,
+                                            download=True)
+
+# Instantiate loader objects to facilitate processing
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                           batch_size = batch_size,
+                                           shuffle = True)
+
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                           batch_size = batch_size,
+                                           shuffle = True)
